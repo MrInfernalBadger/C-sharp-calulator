71b50d90faa8da442e92d33979607b0e3b6f3fbc
MrInfernalBadger <35194014+MrInfernalBadger@users.noreply.github.com>
2024-04-22 15:37:25 +1000

    Basic logic created

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,6 +10,56 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+
+            /*Calculator flow
+             * Intro message
+             * Asks for operation to be completed
+             * Asks for first number
+             * Asks for second number
+             * presents answer and offers second operation on this number
+             */
+
+            Console.WriteLine("Welcome to the Calculator\n");
+
+            Console.WriteLine("1: Addition");
+            Console.WriteLine("2: Subtraction");
+            Console.WriteLine("3: Multiplication");
+            Console.WriteLine("4: Division");
+
+            Console.Write    ("What operation would you like to complete: ");
+            int operation = int.Parse(Console.ReadLine());
+
+            Console.Write    ("What is the first number: ");
+            // ADD TRYPARSE TO CATCH ERRORS
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write    ("What is the second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            float result = 0F;
+
+            switch (operation)
+            {
+                case 1: // Addition
+                    result = firstNumber + secondNumber;
+                    break;
+                case 2: // Subtraction
+                    result = firstNumber - secondNumber;
+                    break;
+                case 3: // Multiplication
+                    result = firstNumber * secondNumber;
+                    break;
+                case 4: // Division
+                    result = firstNumber / secondNumber;
+                    break;
+
+            }
+            string message = $"The final result is {result}";
+            Console.WriteLine(message);
+            Console.ReadLine();
+            /*TODO
+             * Create separate function to bound areas in box for display
+            */
         }
     }
 }
