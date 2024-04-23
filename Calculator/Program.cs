fb904af94f04357580c434388a6f9cefea370487
MrInfernalBadger <35194014+MrInfernalBadger@users.noreply.github.com>
2024-04-23 11:21:12 +1000

    Refactored code to improve readability

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,109 +9,116 @@ namespace Calculator
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            int operation;
-            int firstNumber;
-            int secondNumber;
 
             while (true)
-                {Console.WriteLine("Welcome to the Calculator\n");
-
-                Console.WriteLine("1: Addition");
-                Console.WriteLine("2: Subtraction");
-                Console.WriteLine("3: Multiplication");
-                Console.WriteLine("4: Division");
-
-
-                while (true)
-                {
-                    Console.WriteLine("What operation would you like to complete: ");
-                    if (!int.TryParse(Console.ReadLine(), out operation))
-                    {
-                        Console.WriteLine("Please enter a number between 1 and 4");
-                    } else if (operation < 1 || operation > 4)
-                    {
-                        Console.WriteLine("Please enter a number between 1 and 4");
-                    }
-                    else
-                    {
-                        break;
-                    }
-
-                }
-
+            {
+                Console.WriteLine("Welcome to the Calculator\n");
+                int operation = GetOperationFromUser();
                 Console.Clear();
-                while (true)
-                {
-                    Console.WriteLine("What is the first number: ");
-                    if (!int.TryParse(Console.ReadLine(), out firstNumber)) 
-                    {
-                        Console.WriteLine("Please enter an integer.");
-                    } 
-                    else
-                    {
-                    break;
-                    }
-                }
+                double firstNumber = GetIntegerFromUser("Please enter the first number: ");
                 Console.Clear();
-
-                while (true)
-                {
-                    Console.Write("What is the second number: ");
-                    if (!int.TryParse(Console.ReadLine(), out secondNumber))
-                    {
-                        Console.WriteLine("Please enter an integer.");
-                    }
-                    else if (secondNumber == 0 && operation == 4)
-                    {
-                        Console.WriteLine("Cannot divide by 0, please select a different number.");
-                    }
-                    else
-                    {
-                        break;
-                    }
-
-                }
-
-
-                double result = 0D;
-                char symbol;
-
-                switch (operation)
-                {
-                    case 1: // Addition
-                        result = firstNumber + secondNumber;
-                        symbol = '+';
-                        break;
-                    case 2: // Subtraction
-                        result = firstNumber - secondNumber;
-                        symbol = '-';
-                        break;
-                    case 3: // Multiplication
-                        result = firstNumber * secondNumber;
-                        symbol = '*';
-                        break;
-                    case 4: // Division
-                        result = firstNumber / secondNumber;
-                        symbol = '/';
-                        break;
-                    default:
-                        Console.WriteLine("Some error has occured, please try again\n\n");
-                        symbol = '?';
-                        break;
-
-                }
-                if (symbol != '?')
-                {
+                double secondNumber = GetIntegerFromUser("Please enter the second number: ", operation);
                 Console.Clear();
-                string message = $"The result of {firstNumber} {symbol} {secondNumber} = {result}";
-                Console.WriteLine(message);
-                Console.WriteLine("\n\nPress any key to perform a new sum");
-                Console.ReadLine();
-                Console.Clear();
+                var result = CalculateResult(firstNumber, secondNumber, operation);
+
+
+                if (result.Symbol != '?') // checks if errors has occured in previous step
+                {
+                    DisplayResult(firstNumber, result.Symbol, secondNumber, result.Result);
                 }
             }
 
+        }
+        static void DisplayResult(double firstNumber, char symbol, double secondNumber, double result)
+        {
+            Console.Clear();
+            string message = $"The result of {firstNumber} {symbol} {secondNumber} = {result:0..##}";
+            Console.WriteLine(message);
+            Console.WriteLine("\n\nPress any key to perform a new sum");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        static int GetIntegerFromUser(string prompt, int operation = 0)
+        {
+
+            while (true)
+            {
+                Console.WriteLine(prompt);
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Please enter an integer.");
+                }
+                else if (number == 0 && operation == 4)
+                {
+                    Console.WriteLine("Cannot divide by 0, please select a different number.");
+                }
+                else
+                {
+                    return number;
+                }
+            }
+        }
+
+        static int GetOperationFromUser()
+        {
+            Console.WriteLine("1: Addition");
+            Console.WriteLine("2: Subtraction");
+            Console.WriteLine("3: Multiplication");
+            Console.WriteLine("4: Division");
+
+
+            while (true)
+            {
+                Console.WriteLine("What operation would you like to complete: ");
+                if (!int.TryParse(Console.ReadLine(), out int operation))
+                {
+                    Console.WriteLine("Please enter a number between 1 and 4");
+                }
+                else if (operation < 1 || operation > 4)
+                {
+                    Console.WriteLine("Please enter a number between 1 and 4");
+                }
+                else
+                {
+                    return operation;
+                }
+
+            }
+        }
+
+        static (double Result, char Symbol) CalculateResult(double firstNumber, double secondNumber, int operation)
+        {
+            double result = 0D;
+            char symbol;
+
+            switch (operation)
+            {
+                case 1: // Addition
+                    result = firstNumber + secondNumber;
+                    symbol = '+';
+                    break;
+                case 2: // Subtraction
+                    result = firstNumber - secondNumber;
+                    symbol = '-';
+                    break;
+                case 3: // Multiplication
+                    result = firstNumber * secondNumber;
+                    symbol = '*';
+                    break;
+                case 4: // Division
+                    result = firstNumber / secondNumber;
+                    symbol = '/';
+                    break;
+                default:
+                    Console.WriteLine("Some error has occured, please try again\n\n");
+                    symbol = '?';
+                    break;
+
+            }
+            return (result, symbol);
         }
     }
 }
